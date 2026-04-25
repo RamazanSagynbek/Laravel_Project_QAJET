a1bef1de88c634c2a655299f0c5ede61603f009a
RamazanSagynbek <ramazan@qajet.kz>
2026-04-25 10:27:38 +0600

    Fix dropdown z-index: add relative z-50 to nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="nav-glow glass" style="border-bottom: none;">
+<nav x-data="{ open: false }" class="nav-glow glass relative z-50" style="border-bottom: none;">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
