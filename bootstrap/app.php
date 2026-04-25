0c181533968cfa501c14167973311bbf76fa1697
RamazanSagynbek <ramazan@qajet.kz>
2026-04-25 10:23:07 +0600

    Fix HTTPS forms: trust all proxies for Render reverse proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
             'has.university' => \App\Http\Middleware\EnsureUserHasUniversity::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
